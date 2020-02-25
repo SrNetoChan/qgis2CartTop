14001494367488abab6b30c143430b99bb7f16c2
Nelson Silva <nelson.jgs@gmail.com>
2020-02-25 13:37:58 +0000

    Adds algorithms for themes 2 and 9

---
+++ qgis2CartTop/processing_provider/exportar_mobiliario_urbano_sinal.py
@@ -0,0 +1,187 @@
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsProcessingAlgorithm,
+                       QgsProcessingMultiStepFeedback,
+                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterEnum,
+                       QgsProperty,
+                       QgsProcessingParameterBoolean)
+import processing
+from .utils import get_postgres_connections
+
+
+class Exportar_mob_urbano_sinal(QgsProcessingAlgorithm):
+
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT = 'INPUT'
+    VALOR_TIPO_MOBURBSINAL = 'VALOR_TIPO_MOBURBSINAL'
+    POSTGRES_CONNECTION = 'POSTGRES_CONNECTION'
+
+
+    def initAlgorithm(self, config=None):
+        self.postgres_connections_list = get_postgres_connections()
+
+        self.addParameter(
+            QgsProcessingParameterEnum(
+                self.POSTGRES_CONNECTION,
+                self.tr('Ligação PostgreSQL'),
+                self.postgres_connections_list,
+                defaultValue = 0
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterFeatureSource(
+                self.INPUT,
+                self.tr('Input point/polygon layer (2D)'),
+                types=[QgsProcessing.TypeVectorPoint],
+                defaultValue=None
+            )
+        )
+
+        self.valor_tipo_de_mob_urbano_sinal_dict = {
+            'Armário de semáforos':'1',
+            'Banco de jardim':'2',
+            'Canteiro':'3',
+            'Contentor':'4',
+            'Ecoponto':'5',
+            'Equipamento de exercício físico ao ar livre':'6',
+            'Estacionamento para velocipedes':'7',
+            'Marco de correio':'8',
+            'Painel publicitário':'9',
+            'Papeleira':'10',
+            'Parque infantil':'11',
+            'Parquímetro':'12',
+            'Passadeira de peões':'13',
+            'Placa informativa':'14',
+            'Pérgula':'15',
+            'Posto de carregamento elétrico':'16',
+            'Quiosque fixo':'17',
+            'Sanitário público':'18',
+            'Semáforo':'19',
+            'Sinal de trânsito':'20',
+            'Sinalização':'21'
+
+        }
+
+
+        self.addParameter(
+            QgsProcessingParameterEnum(
+                self.VALOR_TIPO_MOBURBSINAL,
+                self.tr('valor_tipo_de_mob_urbano_sinal'),
+                list(self.valor_tipo_de_mob_urbano_sinal_dict.keys()),
+                defaultValue=1,
+                optional=False,
+            )
+        )
+
+    def processAlgorithm(self, parameters, context, model_feedback):
+        # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
+        # overall progress through the model
+        feedback = QgsProcessingMultiStepFeedback(2, model_feedback)
+        results = {}
+        outputs = {}
+
+        # Convert enumerator to zero based index value
+        valor_local_nomeado = self.parameterAsEnum(
+            parameters,
+            self.VALOR_TIPO_MOBURBSINAL,
+            context
+            ) + 1
+
+        # Refactor fields
+        alg_params = {
+            'FIELDS_MAPPING': [{
+                'expression': 'now()',
+                'length': -1,
+                'name': 'inicio_objeto',
+                'precision': -1,
+                'type': 14
+            },{
+                'expression': str(valor_tipo_de_mob_urbano_sinal),
+                'length': 255,
+                'name': 'valor_tipo_de_mob_urbano_sinal',
+                'precision': -1,
+                'type': 10
+            }],
+            'INPUT': parameters['INPUT'],
+            'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
+        }
+        outputs['RefactorFields'] = processing.run('qgis:refactorfields', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+
+        feedback.setCurrentStep(1)
+        if feedback.isCanceled():
+            return {}
+
+        # Export to PostgreSQL (available connections)
+        idx = self.parameterAsEnum(
+            parameters,
+            self.POSTGRES_CONNECTION,
+            context
+            )
+
+        postgres_connection = self.postgres_connections_list[idx]
+
+        alg_params = {
+            'ADDFIELDS': True,
+            'APPEND': True,
+            'A_SRS': None,
+            'CLIP': False,
+            'DATABASE': postgres_connection,
+            'DIM': 0,
+            'GEOCOLUMN': 'geometria',
+            'GT': '',
+            'GTYPE': 3,
+            'INDEX': True,
+            'INPUT': outputs['RefactorFields']['OUTPUT'],
+            'LAUNDER': True,
+            'OPTIONS': '',
+            'OVERWRITE': False,
+            'PK': '',
+            'PRECISION': True,
+            'PRIMARY_KEY': 'identificador',
+            'PROMOTETOMULTI': True,
+            'SCHEMA': 'public',
+            'SEGMENTIZE': '',
+            'SHAPE_ENCODING': '',
+            'SIMPLIFY': '',
+            'SKIPFAILURES': False,
+            'SPAT': None,
+            'S_SRS': None,
+            'TABLE': 'mob_urbano_sinal',
+            'T_SRS': None,
+            'WHERE': ''
+        }
+        outputs['ExportToPostgresqlAvailableConnections'] = processing.run('gdal:importvectorintopostgisdatabaseavailableconnections', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        return results
+
+    def name(self):
+        return 'exportar_mob_urbano_sinal'
+
+    def displayName(self):
+        return '01. Exportar mobiliário urbano e sinalização'
+
+    def group(self):
+        return '09 - Mobiliário urbano e sinalização'
+
+    def groupId(self):
+        return '09mobiliario'
+
+    def createInstance(self):
+        return Exportar_mob_urbano_sinal()
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def shortHelpString(self):
+        return self.tr("Exporta elementos do tipo mobiliário urbano e sinalização para a base " \
+                       "de dados RECART usando uma ligação PostgreSQL/PostGIS " \
+                       "já configurada.\n\n" \
+                       "A camada vectorial de input deve ser do tipo ponto 2D."
+        )
