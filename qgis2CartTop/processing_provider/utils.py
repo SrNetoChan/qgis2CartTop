a511098d9fa3c288252a892b8790c6bea18792f4
SrNetoChan <senhor.neto@gmail.com>
2020-08-06 19:35:37 +0100

    Better select the list of postgres connections

--- qgis2CartTop/processing_provider/utils.py
+++ qgis2CartTop/processing_provider/utils.py
@@ -5,8 +5,7 @@ def get_postgres_connections():
     s = QSettings()
     s.beginGroup("PostgreSQL/connections")
 
-    d = set([key.split('/')[0] for key in s.allKeys()])
-    d.remove('selected')
+    d = set([key.split('/')[0] for key in s.allKeys() if '/' in key])
     s.endGroup()
 
     return list(d)
