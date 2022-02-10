b63aac2e9ad5e6ecea742c8021edd082b9252218
Alexandre <senhor.neto@gmail.com>
2022-02-10 15:33:01 +0000

    Consolidar curso de agua

--- qgis2CartTop/processing_provider_validacao/provider_validacao.py
+++ qgis2CartTop/processing_provider_validacao/provider_validacao.py
@@ -1,3 +1,4 @@
+import imp
 from qgis.core import QgsProcessingProvider
 from qgis2CartTop.processing_provider_validacao.consolidar_seg_via_rodov import Consolidar_seg_via_rodov
 from .consolidar_curvas_de_nivel import Consolidar_curvas_de_nivel
@@ -5,6 +6,7 @@ from .consolidar_linha_de_quebra import Consolidar_quebra_de_linha
 from .consolidar_fronteira_terra_agua import Consolidar_fronteira_terra_agua
 from .consolidar_seg_via_rodov import Consolidar_seg_via_rodov
 from .consolidar_via_rodov_limite import Consolidar_via_rodov_limite
+from .consolidar_curso_agua_eixo import Consolidar_curso_agua_eixo
 
 class Provider_validacao(QgsProcessingProvider):
 
@@ -14,6 +16,7 @@ class Provider_validacao(QgsProcessingProvider):
         self.addAlgorithm(Consolidar_fronteira_terra_agua())
         self.addAlgorithm(Consolidar_seg_via_rodov())
         self.addAlgorithm(Consolidar_via_rodov_limite())
+        self.addAlgorithm(Consolidar_curso_agua_eixo())
         # add additional algorithms here
         # self.addAlgorithm(MyOtherAlgorithm())
 
